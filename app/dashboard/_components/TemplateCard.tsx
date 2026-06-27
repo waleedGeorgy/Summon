3ef8d06a7ee32f6d7f416641ae24cbd91d224ffd
Waleed Georgy <waleedgeorge1009@gmail.com>
2026-06-27 20:31:55 +0300

    Bug fix - Fix description rendering in DashboardCard and TemplateCard components

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -42,7 +42,6 @@ const tailwindColors: Record<string, string> = {
 };
 
 const TemplateCard = ({ template }: { template: Template }) => {
-    console.log(template)
     const IconComponent = icons[template.icon as keyof typeof icons] as LucideIcon;
     const hexColor = tailwindColors[template.color] || "#64748b";
 
@@ -112,13 +111,13 @@ const TemplateCard = ({ template }: { template: Template }) => {
                     }
                 </CardAction>
             </CardHeader>
-            {template.description && template.description?.length > 0 ?
-                <CardContent className="line-clamp-2 leading-relaxed max-w-70">
-                    {template.description}
-                </CardContent>
-                :
-                <span className="italic text-neutral-400">No description</span>
-            }
+            <CardContent className="line-clamp-2 leading-relaxed max-w-70">
+                {template.description && template.description?.length > 0 ?
+                    template.description 
+                    :
+                    <span className="italic text-neutral-400">No description</span>
+                }
+            </CardContent>
         </Card>
     )
 }
